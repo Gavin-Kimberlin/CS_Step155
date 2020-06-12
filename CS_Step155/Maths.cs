08060c462d2f12dcc862766c9c8cbb2777c99d3f
Gavin Kimberlin <guviner@gmail.com>
2020-06-12 03:55:39 -0700

    Create a math program using method overloading.

---
+++ CS_Step155/Maths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Step155
+{
+    class Maths
+    {
+        public static int Method(int input)
+        {
+            int output = input * 226;
+            return output;
+        }
+        public static decimal Method(decimal input)
+        {
+            int output = Convert.ToInt32(input) / 2;
+            return output;
+        }
+        public static double Method(string input)
+        {
+            int output = Convert.ToInt32(input) * Convert.ToInt32(input);
+            return output;
+        }
+    }
+}
--- CS_Step155/Program.cs
+++ CS_Step155/Program.cs
@@ -10,6 +10,13 @@ namespace CS_Step155
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n                                                        Math App 2\n=======================================================================================================================");
+            Console.WriteLine("Enter a number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(userNum + " times 226 is " + Maths.Method(userNum));
+            Console.WriteLine(userNum + " divided by 2 is " + Maths.Method(Convert.ToDecimal(userNum)));
+            Console.WriteLine(userNum + " squared is " + Maths.Method(userNum.ToString()));
+            Console.ReadLine();
         }
     }
 }
